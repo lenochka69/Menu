6e8021a10cad74e33df604324cdd6975bed0de66
lenochka69 <93248482+lenochka69@users.noreply.github.com>
2021-11-15 17:57:34 +0300

    commit3

--- app/src/main/java/com/example/menu/BreakfastFragment.kt
+++ app/src/main/java/com/example/menu/BreakfastFragment.kt
@@ -1,5 +1,7 @@
 package com.example.menu
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -15,6 +17,12 @@ class Breakfast : Fragment() {
     //ранняя инициализация переменных для вью
     private lateinit var diceImageView: ImageView
     private lateinit var randomizeButton: Button
+    private lateinit var buttonMilK: Button
+    private lateinit var buttonPorridge:Button
+    private lateinit var buttonPancakes:Button
+    private lateinit var buttonCheese:Button
+    private lateinit var buttonEggs:Button
+    private lateinit var buttonMuesli: Button
 
     private var randomValue = 1
 
@@ -28,6 +36,50 @@ class Breakfast : Fragment() {
         savedInstanceState?.let { randomValue = it.getInt(LAST_RANDOM_VALUE) }
         rollDice(randomValue)
 
+        buttonMilK=view.findViewById(R.id.milk)
+        buttonPorridge = view.findViewById(R.id.porridge)
+        buttonPancakes = view.findViewById(R.id.pancakes)
+        buttonEggs = view.findViewById(R.id.eggs)
+        buttonCheese = view.findViewById(R.id.cheese)
+        buttonMuesli = view.findViewById(R.id.muesli)
+
+        val linkMilk = Uri.parse("https://www.photorecept.ru/smart-filter/zavtrak-iz-moloka")
+        val intentMilk = Intent(Intent.ACTION_VIEW,linkMilk)
+        buttonMilK.setOnClickListener{
+            startActivity(Intent(intentMilk))
+        }
+        val linkPorridge = Uri.parse("https://www.russianfood.com/recipes/bytype/?fid=489,926")
+        val intentPorridge = Intent(Intent.ACTION_VIEW,linkPorridge)
+        buttonPorridge.setOnClickListener {
+            startActivity(Intent(intentPorridge))
+
+        }
+
+        val linkPancakes = Uri.parse("https://www.russianfood.com/recipes/bytype/?fid=810,926")
+        val intentPancakes = Intent(Intent.ACTION_VIEW,linkPancakes)
+        buttonPancakes.setOnClickListener {
+            startActivity(Intent(intentPancakes))
+        }
+
+        val linkEggs = Uri.parse("https://www.russianfood.com/recipes/bytype/?fid=97,926")
+        val intentEggs = Intent(Intent.ACTION_VIEW,linkEggs)
+        buttonEggs.setOnClickListener {
+            startActivity(Intent(intentEggs))
+        }
+
+        val linkCheese = Uri.parse("https://www.russianfood.com/recipes/bytype/?fid=926,1075")
+        val intentCheese = Intent(Intent.ACTION_VIEW,linkCheese)
+        buttonCheese.setOnClickListener {
+            startActivity(Intent(intentCheese))
+        }
+
+        val linkMuesli = Uri.parse("https://www.povarenok.ru/recipes/category/384/")
+        val intentMuesli = Intent(Intent.ACTION_VIEW, linkMuesli)
+        buttonMuesli.setOnClickListener {
+            startActivity(Intent(intentMuesli))
+        }
+
+
         randomizeButton.setOnClickListener {
             randomValue = Random.nextInt(1, 6)
             rollDice(randomValue)
@@ -35,6 +87,7 @@ class Breakfast : Fragment() {
 
         return view
     }
+
 
     //подкидывает кубик
     private fun rollDice(randomValue: Int) {
@@ -50,7 +103,6 @@ class Breakfast : Fragment() {
             }
         )
     }
-
     //сохраняет состояние
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
